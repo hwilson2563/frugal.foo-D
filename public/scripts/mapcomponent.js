d0e0aea73b07b7a41462d5fe7e73ecd05f328d0b
Julie Frederick <32311073+julesfred@users.noreply.github.com>
2017-12-04 15:31:42 -0500

    Update mapcomponent.js

--- public/scripts/mapcomponent.js
+++ public/scripts/mapcomponent.js
@@ -5,7 +5,13 @@
         var vm = this;
         vm.budget=BudgetService.getBudget();
         console.log(vm.budget);
+        BudgetService.getLocation().then(function(response){
+            console.log(response);
+        })
+
     }
+
+    
 };
     angular
     .module("app")
