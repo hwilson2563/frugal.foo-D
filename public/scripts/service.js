5ae412185f5309c23bf5339e093bd9d6bb5aa9e2
Morgan McPeak <mcpeakmorgan@gmail.com>
2017-12-04 10:57:11 -0500

    service made

---
+++ public/scripts/service.js
@@ -0,0 +1,18 @@
+(function(){
+    function BudgetService(){
+        var word = {};
+    return {
+        setBudget: setBudget,
+        getBudget: getBudget
+    };
+    function setBudget() {
+        
+    }
+    function getBudget(){
+        
+    }
+    }
+    angular 
+    .module("app")
+    .factory("BudgetService", BudgetService);
+})();
