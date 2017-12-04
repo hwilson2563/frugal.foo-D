ff6ba09fd8b72d18ee3d7794c9a7a8c2f6f07039
Julie Frederick <32311073+julesfred@users.noreply.github.com>
2017-12-04 10:29:38 -0500

    Add files via upload

---
+++ public/scripts/homecomponent.js
@@ -0,0 +1,11 @@
+(function() {
+    var listComponent = {
+      templateUrl: "index.html",
+      controller: function(service) {
+
+      }
+  
+    angular
+      .module("app")
+      .component("homComponent", homComponent);
+})();
