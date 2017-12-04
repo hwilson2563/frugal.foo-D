80b926fe998a160eeb824e3292522f81b85f88c6
Morgan McPeak <mcpeakmorgan@gmail.com>
2017-12-04 10:24:17 -0500

    Update profilecomponent.js

---
+++ public/scripts/profilecomponent.js
@@ -0,0 +1,9 @@
+(function(){
+    var profileComponent ={
+    //controller:
+    templateUrl: "profile.html"
+};
+    angular
+    .module("app")
+    .component("profileComponent", profileComponent);
+})();
