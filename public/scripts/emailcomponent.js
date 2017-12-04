e9a2b38cc207873c1e4aec9bc94121a4ce813ed1
Morgan McPeak <mcpeakmorgan@gmail.com>
2017-12-04 10:26:08 -0500

    Update emailcomponent.js

---
+++ public/scripts/emailcomponent.js
@@ -0,0 +1,9 @@
+(function(){
+    var emailComponent ={
+    //controller:
+    templateUrl: "email.html"
+};
+    angular
+    .module("app")
+    .component("emailComponent", emailComponent);
+})();
