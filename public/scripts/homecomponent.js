55ff132e9635bdabbb177a479ec34d4c9587b274
Julie Frederick <32311073+julesfred@users.noreply.github.com>
2017-12-04 11:33:29 -0500

    Update homecomponent.js

--- public/scripts/homecomponent.js
+++ public/scripts/homecomponent.js
@@ -1,14 +1,17 @@
 (function(){
 	var homeComponent={
-	templateUrl:"index.html",
+	templateUrl:"home.html",
 	controller: function(BudgetService){
 		var vm = this;
         vm.budget = 0;
-        vm.submitBudget = function(){
+        vm.submitBudget = function(budget){
+          vm.budget = budget;
+          console.log(vm.budget);
     	BudgetService.submitBudget(budget).then(function(response){vm.budget = response.data});
         };
-	}
-angular
+      }
+    }
+      angular
       .module("app")
-      .component("homeComponent", homeComponent);
-}})();
+      .component("homeComponent", homeComponent)
+})();
