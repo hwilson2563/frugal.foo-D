cbfe27673181750683cec1edd7c0638a6d1c46b5
Hilary Wilson <32081469+hwilson2563@users.noreply.github.com>
2017-12-05 17:06:40 -0500

    Update service.js
    get http

--- public/scripts/service.js
+++ public/scripts/service.js
@@ -1,49 +1,45 @@
 (function(){
-    function BudgetService($http){
-        var budget = 0;
-        var updateBudget = 0;
-        var apiData ="";
-    return {
-        setBudget: setBudget,
-        getBudget: getBudget,
-        // getLocation: getLocation,
-        newBudget: newBudget,
-        getUpdateBudget: getUpdateBudget
-    };
-    function getUpdateBudget() {
-    return updateBudget;     
-    }
-    function setBudget(newBudget) {
-        budget = newBudget;
-        updateBudget = newBudget;
-        console.log(budget);
-        
-    }
-    function getBudget(){
-        return budget;
-    } 
-
-//     function getLocation(){
-
-//         return $http({
-//             url: "https://maps.googleapis.com/maps/api/geocode/json?latlng=&key=AIzaSyAngDeThvs8q0Pucu537zmfRT8JNA6ydbs",
-//             method: "GET"
-//         }).then(function(response){
-//             console.log(response.data);
-//             apiData=response;
-//             return apiData;
-//         });
-
-//    }
-   function newBudget(objPrice){
-        updateBudget = updateBudget - objPrice;
-        return updateBudget;
-   }
+  function BudgetService($http){
+    var budget = 0;
+    var updateBudget = 0;
+    var apiData ="";
     
-
-
+    return {
+      setBudget: setBudget,
+      getBudget: getBudget,
+      getLocation: getLocation,
+      newBudget: newBudget,
+      getUpdateBudget: getUpdateBudget
+    };
+  
+    function getUpdateBudget() {
+      return updateBudget;     
     }
-    angular 
+    
+    function setBudget(newBudget) {
+      budget = newBudget;
+      updateBudget = newBudget;
+      console.log(budget); 
+    }
+  
+    function getBudget(){
+      return budget;
+    } 
+    function getLocation(lat, lng){
+       return $http({
+            url: "https://maps.googleapis.com/maps/api/geocode/json?latlng="+lat+","+lng+"&key=AIzaSyClnD3HQ6uqUslLP8aorLZ53am9ggg8bHc",
+            method: "GET"
+        }).then(function(response){
+            console.log(response.data);
+       })
+}
+    function newBudget(objPrice){
+      updateBudget = updateBudget - objPrice;
+      return updateBudget;
+    } 
+  }
+  
+  angular 
     .module("app")
     .factory("BudgetService", BudgetService);
 })();
