d89e9cce81368be71bcf0d7f578f8f59a6d3f464
Morgan McPeak <mcpeakmorgan@gmail.com>
2017-12-05 13:59:47 -0500

    Update mapcomponent.js

--- public/scripts/mapcomponent.js
+++ public/scripts/mapcomponent.js
@@ -5,23 +5,23 @@
         var vm = this;
 
         vm.initMap =(function() {
-            var locations = [
-                {lat: 42.331598, lng: -83.065734},
-                {lat: 42.331675, lng: -83.070876},
-                {lat: 42.331597, lng: -83.075984},
-                {lat: 42.329803, lng: -83.064864},
-                {lat: 42.331145, lng: -83.076160},
-                {lat: 42.331603, lng: -83.075377},
-                {lat: 42.331684, lng: -83.080834},
-                {lat: 42.331589, lng: -83.069892},
-                {lat: 42.331154, lng: -83.073921},
-                {lat: 42.328422, lng: -83.063397},
-                {lat: 42.321868, lng: -83.071471},
-                {lat: 42.331148, lng: -83.073919},
-                {lat: 42.331713, lng: -83.063237},
-                {lat: 42.331595, lng: -83.065400},
-                {lat: 42.331616, lng: -83.066820}
-            ]
+     var locations = [
+                        {name: "Ottava Via", lat: 42.331598, lng: -83.065734},
+                        {name: "Bobcat Bonnies", lat: 42.331675, lng: -83.070876},
+                        {name: "Slows", lat: 42.331597, lng: -83.075984},
+                        {name: "Lady of the house", lat: 42.329803, lng: -83.064864},
+                        {name: "Mercury Bar", lat: 42.331145, lng: -83.076160},
+                        {name: "Gold Cash Gold", lat: 42.331603, lng: -83.075377},
+                        {name: "Takoi", lat: 42.331684, lng: -83.080834},
+                        {name: "Corktown Tavern", lat: 42.331589, lng: -83.069892},
+                        {name: "Ima",lat: 42.331154, lng: -83.073921},
+                        {name: "Batch", lat: 42.328422, lng: -83.063397},
+                        {name: "Green Dot", lat: 42.321868, lng: -83.071471},
+                        {name: "Rubbed", lat: 42.331148, lng: -83.073919},
+                        {name: "Pj's Lager House", lat: 42.331713, lng: -83.063237},
+                        {name: "Nemo's", lat: 42.331595, lng: -83.065400},
+                        {name: "McShanes", lat: 42.331616, lng: -83.066820}
+                  ];
             
             var map = new google.maps.Map(document.getElementById('map'), {
                 zoom: 15,
