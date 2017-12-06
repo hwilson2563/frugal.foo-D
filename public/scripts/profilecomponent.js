260c0c265a7a58ee684042f8be4e27b42860a2e7
Julie Frederick <32311073+julesfred@users.noreply.github.com>
2017-12-06 14:54:27 -0500

    Update profilecomponent.js

--- public/scripts/profilecomponent.js
+++ public/scripts/profilecomponent.js
@@ -1,7 +1,11 @@
 (function(){
     var profileComponent ={
-    //controller:
-    templateUrl: "profile.html"
+    templateUrl: "profile.html",
+    controller: function(BudgetService){
+        var vm = this;
+        vm.info = BudgetService.getInfo();
+        console.log(vm.info);
+    }
 };
     angular
     .module("app")
