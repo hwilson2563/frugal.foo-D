b152e1035a3a2e90728acd82019c26b0b3b063e4
Julie Frederick <32311073+julesfred@users.noreply.github.com>
2017-12-06 14:54:02 -0500

    Update service.js

--- public/scripts/service.js
+++ public/scripts/service.js
@@ -1,45 +1,58 @@
 (function(){
-  function BudgetService($http){
-    var budget = 0;
-    var updateBudget = 0;
-    var apiData ="";
+    function BudgetService($location){
+      var budget = 0;
+      var updateBudget = 0;
+      var apiData ="";
+      var locationInfo = {};
+      
+      return {
+        setBudget: setBudget,
+        getBudget: getBudget,
+        getLocation: getLocation,
+        newBudget: newBudget,
+        getUpdateBudget: getUpdateBudget,
+        submitInfo: submitInfo,
+        getInfo: getInfo
+      };
+
+      function getInfo(){
+          return locationInfo;
+      }
+
+      function submitInfo(info){
+          locationInfo = info;
+          console.log(locationInfo);
+          $location.path("/profile");
+      }
     
-    return {
-      setBudget: setBudget,
-      getBudget: getBudget,
-      getLocation: getLocation,
-      newBudget: newBudget,
-      getUpdateBudget: getUpdateBudget
-    };
-  
-    function getUpdateBudget() {
-      return updateBudget;     
-    }
+      function getUpdateBudget() {
+        return updateBudget;     
+      }
+      
+      function setBudget(newBudget) {
+        budget = newBudget;
+        updateBudget = newBudget;
+        console.log(budget); 
+      }
     
-    function setBudget(newBudget) {
-      budget = newBudget;
-      updateBudget = newBudget;
-      console.log(budget); 
-    }
-  
-    function getBudget(){
-      return budget;
-    } 
-    function getLocation(lat, lng){
-//        return $http({
-//             url: "https://maps.googleapis.com/maps/api/geocode/json?latlng="+lat+","+lng+"&key=AIzaSyClnD3HQ6uqUslLP8aorLZ53am9ggg8bHc",
-//             method: "GET"
-//         }).then(function(response){
-//             console.log(response.data);
-//        })
-}
-    function newBudget(objPrice){
-      updateBudget = updateBudget - objPrice;
-      return updateBudget;
-    } 
+      function getBudget(){
+        return budget;
+      } 
+      function getLocation(lat, lng){
+  //        return $http({
+  //             url: "https://maps.googleapis.com/maps/api/geocode/json?latlng="+lat+","+lng+"&key=AIzaSyClnD3HQ6uqUslLP8aorLZ53am9ggg8bHc",
+  //             method: "GET"
+  //         }).then(function(response){
+  //             console.log(response.data);
+  //        })
   }
-  
-  angular 
-    .module("app")
-    .factory("BudgetService", BudgetService);
-})();
+      function newBudget(objPrice){
+        updateBudget = updateBudget - objPrice;
+        return updateBudget;
+      } 
+    }
+    
+    angular 
+      .module("app")
+      .factory("BudgetService", BudgetService);
+  })();
