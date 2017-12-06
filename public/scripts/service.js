a0aa5ad40434e1b941cef87b7aceee4226fa2461
Hilary Wilson <32081469+hwilson2563@users.noreply.github.com>
2017-12-06 11:24:41 -0500

    Update service.js
    comment out

--- public/scripts/service.js
+++ public/scripts/service.js
@@ -26,12 +26,12 @@
       return budget;
     } 
     function getLocation(lat, lng){
-       return $http({
-            url: "https://maps.googleapis.com/maps/api/geocode/json?latlng="+lat+","+lng+"&key=AIzaSyClnD3HQ6uqUslLP8aorLZ53am9ggg8bHc",
-            method: "GET"
-        }).then(function(response){
-            console.log(response.data);
-       })
+//        return $http({
+//             url: "https://maps.googleapis.com/maps/api/geocode/json?latlng="+lat+","+lng+"&key=AIzaSyClnD3HQ6uqUslLP8aorLZ53am9ggg8bHc",
+//             method: "GET"
+//         }).then(function(response){
+//             console.log(response.data);
+//        })
 }
     function newBudget(objPrice){
       updateBudget = updateBudget - objPrice;
