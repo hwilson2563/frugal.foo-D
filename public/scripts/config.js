c41425d65647307cd72bf000745622c1df2436fb
Julie Frederick <32311073+julesfred@users.noreply.github.com>
2017-12-06 14:58:04 -0500

    Update config.js

--- public/scripts/config.js
+++ public/scripts/config.js
@@ -12,6 +12,18 @@
             .when("/map", {
                 template:"<map-component></map-component>"
             })
+            .when("/profile", {
+                template:"<profile-component></profile-component>"
+            })
+            .when("/menu", {
+                template:"<menu-component></menu-component>"
+            })
+            .when("/cart", {
+                template:"<cart-component></cart-component>"
+            })
+            .when("/email", {
+                template:"<email-component></email-component>"
+            })
             .otherwise({ redirectTo: "/home" });
         });
 })();
