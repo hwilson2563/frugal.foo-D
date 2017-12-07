d34839f26a49675791943dbe6141f9ed73ed45cb
Hilary Wilson <32081469+hwilson2563@users.noreply.github.com>
2017-12-07 15:35:03 -0500

    Update menucomponent.js
    running if statement to show div

--- public/scripts/menucomponent.js
+++ public/scripts/menucomponent.js
@@ -3,11 +3,14 @@
     templateUrl: "menu.html",
     controller: function(BudgetService){
         var vm = this;
+        document.getElementById("overbudget").style.display = "none";
         vm.menu=[];
     	vm.initialBudget= BudgetService.getBudget();
         vm.budget= BudgetService.getUpdateBudget(); 
         vm.info = BudgetService.getInfo();
-     
+        vm.overbudgetRemove = function() {
+            document.getElementById("overbudget").style.display = "none";
+        }
 
         BudgetService.getFoodList().then(function(response){
             vm.menu = response.data;
@@ -18,10 +21,14 @@
             console.log(addItem);
             BudgetService.addCart(addItem);
             vm.budget= BudgetService.getUpdateBudget();
-            if(vm.budget <= 0){
+            if(vm.budget < 0){
                 document.getElementById("overbudget").style.display = "block";
                 BudgetService.removeCart(addItem);
                 vm.budget= BudgetService.getUpdateBudget();
+                return;
+            }
+            else {
+                document.getElementById("overbudget").style.display = "none";
             }
         };
 
