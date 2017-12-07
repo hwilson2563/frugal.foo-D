60d22174d6f7b9590f46739946dbc9de9447d554
Hilary Claire <hilary.wilson2563@gmail.com>
2017-12-07 17:20:47 -0500

    add tip calculator to function

--- public/scripts/cartcomponent.js
+++ public/scripts/cartcomponent.js
@@ -7,12 +7,28 @@
         vm.budget= BudgetService.getUpdateBudget(); 
         vm.info = BudgetService.getInfo();
         vm.tab = BudgetService.getCart();
+        vm.tip = "";
 
         vm.remove= function(index, items){
             BudgetService.deleteFood(items);
             vm.tab.splice(index, 1);
             vm.budget = BudgetService.getUpdateBudget();
         }
+        vm.tipOne =function(){
+          document.getElementById("tipbar").style.display = "block";
+          var total = vm.initialBudget-vm.budget ;
+           vm.tip=( total * .15);
+        };
+        vm.tipTwo =function(){
+            document.getElementById("tipbar").style.display = "block";
+            var total = vm.initialBudget-vm.budget ;
+           vm.tip=( total * .20);
+        };        
+        vm.tipThree =function(){
+            document.getElementById("tipbar").style.display = "block";
+             var total = vm.initialBudget-vm.budget ;
+           vm.tip=( total * .25);
+        };
     }
 };
     angular
