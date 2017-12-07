73d7cd1a5349fdc0df98aaaab2dfbdbf5dce3f09
Julie Frederick <32311073+julesfred@users.noreply.github.com>
2017-12-07 15:58:48 -0500

    Update emailcomponent.js

--- public/scripts/emailcomponent.js
+++ public/scripts/emailcomponent.js
@@ -1,8 +1,23 @@
 (function(){
     var emailComponent ={
-    //controller:
-    templateUrl: "email.html"
-};
+    templateUrl: "email.html",
+    controller: function (BudgetService, $http){
+        var vm = this;
+    	vm.initialBudget= BudgetService.getBudget();
+        vm.budget= BudgetService.getUpdateBudget(); 
+        vm.info = BudgetService.getInfo();
+        vm.tab = BudgetService.getCart();
+        console.log($http);
+
+       vm.sendEmail = function(data){
+        $http.post("/email", data);
+        
+       }
+
+    }
+
+}
+    
     angular
     .module("app")
     .component("emailComponent", emailComponent);
