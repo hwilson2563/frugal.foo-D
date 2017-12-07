67d162960c6da51fe1892ec5158ec9013adda2f8
Hilary Wilson <32081469+hwilson2563@users.noreply.github.com>
2017-12-07 13:54:51 -0500

    Update profilecomponent.js
    adding image to page

--- public/scripts/profilecomponent.js
+++ public/scripts/profilecomponent.js
@@ -5,8 +5,9 @@
         var vm = this;
         vm.info = BudgetService.getInfo();
         console.log(vm.info);
-           vm.initialBudget= BudgetService.getBudget();
+        vm.initialBudget= BudgetService.getBudget();
         vm.budget= BudgetService.getUpdateBudget();
+        vm.img = BudgetService.getPhoto();
     }
 };
     angular
